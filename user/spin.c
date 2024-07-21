015c24e2951ab1d0b5c32c0a6971796fa44ebd4c
EdenMoxe <2436444815@qq.com>
2024-07-21 01:47:55 -0700

    Test1 Uthread Done

---
+++ user/spin.c
@@ -0,0 +1,22 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc,char *arvg[]){
+	int pid;
+	char c;
+
+	pid=fork();
+	if(pid==0){
+		c = '/';
+	}
+	else{
+		printf("parent pid is %d,child pid is %d",getpid(),pid);
+		c='\\';
+	}
+    
+	for(int i =0;;i++){
+		if((i%1000000)==0)
+			write(2,&c,1);
+	}
+	exit(0);
+}
